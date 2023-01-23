1e18388b3904e40c5a5c61acd8799a0a0998f675
Ivan Rakcheev <rakcheevib@alfastrah.ru>
2023-01-23 16:49:20 +0300

    Solve sum & CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp} from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+
+app.mount('#app');
