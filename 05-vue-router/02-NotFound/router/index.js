a13d9fdd26b27152b1041c6043f4054e4d8630a5
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-02-10 12:24:21 +0300

    Added solution for task: 404 - NotFound

--- 05-vue-router/02-NotFound/router/index.js
+++ 05-vue-router/02-NotFound/router/index.js
@@ -12,5 +12,9 @@ export const router = createRouter({
       path: '/page-b',
       component: () => import('../views/PageB'),
     },
+    {
+      path: '/:unknownPath(.*)',
+      component: () => import('../views/PageNotFound'),
+    },
   ],
 });
