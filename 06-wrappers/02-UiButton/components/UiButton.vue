2dd9d91ded90974af12fc6b8e3f143ac6d332f6f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-02-16 10:24:29 +0300

    Added solution for task: UiButton

--- 06-wrappers/02-UiButton/components/UiButton.vue
+++ 06-wrappers/02-UiButton/components/UiButton.vue
@@ -1,10 +1,57 @@
 <template>
-  <button class="button button_secondary button_block">BUTTON</button>
+  <component :is="tag" class="button" :class="[variantClass, { button_block: block }]" :type="buttonType">
+    <slot />
+  </component>
 </template>
 
 <script>
+// Опишем отдельно соотношение между вариантами кнопок и классами.
+// Если бы вёрстка не была готовой, проще было бы делать классы эквивалентными вариантам,
+// но работаем с тем, что имеем.
+const buttonClasses = {
+  primary: 'button_primary',
+  secondary: 'button_secondary',
+  danger: 'button_danger',
+};
+
 export default {
   name: 'UiButton',
+
+  props: {
+    tag: {
+      type: [String, Object, Function],
+      default: 'button',
+    },
+
+    block: {
+      type: Boolean,
+      default: false,
+    },
+
+    variant: {
+      type: String,
+      default: 'secondary',
+      validator: (value) => Object.keys(buttonClasses).includes(value),
+    },
+
+    type: {
+      type: String,
+    }
+  },
+
+  computed: {
+    variantClass() {
+      return buttonClasses[this.variant];
+    },
+
+    buttonType() {
+      // Значением параметра type делаем `button` по умолчанию для HTML кнопок
+      if (this.tag === 'button') {
+        return this.type ?? 'button';
+      }
+      return this.type;
+    },
+  },
 };
 </script>
 
