9f3a42f73ea7901d8e547ad7cc180ad61eb892f6
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-02-22 09:00:25 +0300

    Added task VNode

---
+++ 09-rendering/01-VNode/components/VNode.vue
@@ -0,0 +1,15 @@
+<script>
+import { isVNode } from 'vue';
+
+export default {
+  name: 'VNode',
+
+  props: {
+    vnode: {
+      type: [Object, Array],
+      require: true,
+      validator: (value) => (Array.isArray(value) ? value.every((item) => isVNode(item)) : isVNode(value)),
+    },
+  },
+};
+</script>
