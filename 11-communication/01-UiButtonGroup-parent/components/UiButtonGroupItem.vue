7a58f054ab0a4cb62b67d946307aa561065d229f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-02-27 09:24:00 +0300

    Added task UiButtonGroup через $parent

---
+++ 11-communication/01-UiButtonGroup-parent/components/UiButtonGroupItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <button class="button-group__button button-group__button_active" type="button" aria-selected="false">Button</button>
+</template>
+
+<script>
+export default {
+  name: 'UiButtonGroupItem',
+
+  props: {
+    value: {
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button-group__button {
+  background-color: var(--white);
+  border: 2px solid var(--blue);
+  border-left: none;
+  height: 44px;
+  padding: 0 8px;
+  color: var(--blue);
+  font-weight: 400;
+  font-size: 20px;
+  font-family: inherit;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition-duration: 0.3s;
+  transition-property: background-color, fill;
+  box-shadow: none;
+  outline: none;
+  cursor: pointer;
+}
+
+.button-group__button:first-child {
+  border-radius: 22px 0 0 22px;
+  border-left: 2px solid var(--blue);
+  padding-left: 10px;
+  padding-right: 6px;
+}
+
+.button-group__button:last-child {
+  border-radius: 0 22px 22px 0;
+  padding-right: 10px;
+}
+
+.button-group__button:hover,
+.button-group__button_active {
+  background-color: var(--blue);
+  color: var(--white);
+}
+
+.button-group__button :slotted(svg) {
+  fill: var(--blue);
+}
+
+.button-group__button:hover :slotted(svg),
+.button-group__button_active :slotted(svg) {
+  fill: var(--white);
+}
+</style>
