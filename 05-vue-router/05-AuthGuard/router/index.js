fa05480765f2726ecfbda3d780744a80133b9043
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-03-16 20:40:56 +0300

    Added solution for task: AuthGuard

--- 05-vue-router/05-AuthGuard/router/index.js
+++ 05-vue-router/05-AuthGuard/router/index.js
@@ -1,16 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { isAuthenticated } from '../services/authService.js';
 
+/** @implements {import('vue-router').NavigationGuard} */
+function authGuard(to) {
+  // Гард принимает на вход следующий и предыдущий маршруты. Но нам нужен только следующий
+
+  // Если маршрут требует авторизации, а пользователь не авторизован - переходим на страницу авторизации
+  if (to.meta.requireAuth && !isAuthenticated()) {
+    // В query параметр from передаём путь запрашиваемой страницы
+    return { name: 'login', query: { from: to.fullPath } };
+  }
+
+  // Если маршрут требует отсутствия авторизации, а пользователь авторизован - переходим на главную страницу
+  if (to.meta.requireGuest && isAuthenticated()) {
+    return { name: 'index' };
+  }
+
+  // В остальных случаях разрешаем переход без ограничений
+  return true;
+}
+
 const router = createRouter({
   history: createWebHistory('/05-vue-router/05-AuthGuard'),
   routes: [
     {
       path: '/',
+      name: 'index',
       alias: '/meetups',
       component: () => import('../views/PageMeetups.vue'),
     },
     {
       path: '/login',
+      name: 'login',
       meta: {
         requireGuest: true,
       },
@@ -18,6 +39,7 @@ const router = createRouter({
     },
     {
       path: '/register',
+      name: 'register',
       meta: {
         requireGuest: true,
       },
@@ -25,6 +47,7 @@ const router = createRouter({
     },
     {
       path: '/meetups/create',
+      name: 'meetup-create',
       meta: {
         requireAuth: true,
       },
@@ -32,6 +55,7 @@ const router = createRouter({
     },
     {
       path: '/meetups/:meetupId(\\d+)/edit',
+      name: 'meetup-edit',
       meta: {
         requireAuth: true,
       },
@@ -39,5 +63,8 @@ const router = createRouter({
     },
   ],
 });
+
+// Используем beforeEach гард, чтобы проверять пользователя перед каждым переходом
+router.beforeEach(authGuard);
 
 export { router };
