27cee199542a8d6bd238c9a7c926a907ffb095a4
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-03-16 20:29:16 +0300

    Added solution for task: ProgressPlugin

--- 13-reusability/01-ProgressPlugin/main.js
+++ 13-reusability/01-ProgressPlugin/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import { router } from './router/index.js';
 import { createProgress } from './plugins/progress/index.js';
 
+// Создаём прогресс
 const progress = createProgress({ container: '#progress', router });
-
-createApp(App).use(router).mount('#app');
+// Устанавливаем в приложение
+createApp(App).use(router).use(progress).mount('#app');
