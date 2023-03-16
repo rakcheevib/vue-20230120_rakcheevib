ce0200d4ed55700a270be29a927164e20ac767f7
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-03-16 19:42:32 +0300

    Added task UiButtonGroup через provide/inject

---
+++ 12-provide-inject/01-UiButtonGroup-provide/components/UiButtonGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button-group" role="group">
+    <!-- Эти кнопки должны передаваться через слот -->
+    <button class="button-group__button button-group__button_active" type="button" aria-selected="true">Button1</button>
+    <button class="button-group__button" type="button" aria-selected="false">Button2</button>
+    <button class="button-group__button" type="button" aria-selected="false">Button3</button>
+    <!-- Эти кнопки должны передаваться через слот -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiButtonGroup',
+
+  props: {
+    modelValue: {
+      required: true,
+    },
+  },
+
+  emits: ['update:modelValue'],
+};
+</script>
+
+<style scoped>
+.button-group {
+  display: inline-flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  white-space: nowrap;
+}
+</style>
